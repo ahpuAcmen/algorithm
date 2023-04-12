0f51397ed247a1c9ff55b6abf2858b10560164a2
wangjun <wangjun@allcam.com.cn>
2023-04-13 00:32:25 +0800

    合并有序数组

--- src/leetcode/LongestStringWithoutRepeating.java
+++ src/leetcode/LongestStringWithoutRepeating.java
@@ -14,42 +14,32 @@ public class LongestStringWithoutRepeating {
         ArrayList<Character> list = new ArrayList<>();
         ArrayList<Integer> integers = new ArrayList<>();
         int[] repeatSite = new int[s.length()];
-        for (int i = 0; i < chars.length; i++)
-        {
-            if (list.contains(chars[i]))
-            {
+        for (int i = 0; i < chars.length; i++) {
+            if (list.contains(chars[i])) {
                 int last = list.lastIndexOf(chars[i]);
                 repeatSite[i] = last;
                 integers.add(i);
-            } else
-            {
+            } else {
                 repeatSite[i] = -1;
             }
             list.add(chars[i]);
         }
 
-        if (integers.size() == 0)
-        {
+        if (integers.isEmpty()) {
             return s.length();
         }
         int longestLength = 0;
         int start = 0;
-        for (Integer site : integers)
-        {
-            if (longestLength == 0)
-            {
+        for (Integer site : integers) {
+            if (longestLength == 0) {
                 longestLength = site;
-            } else
-            {
+            } else {
                 int last = repeatSite[site];
                 int length = site - last;
-                if (length > longestLength && site - start + 1 > longestLength)
-                {
-                    if (last >= start)
-                    {
+                if (length > longestLength && site - start + 1 > longestLength) {
+                    if (last >= start) {
                         longestLength = length;
-                    } else
-                    {
+                    } else {
                         longestLength = site - start + 1;
                     }
                 }
@@ -58,15 +48,27 @@ public class LongestStringWithoutRepeating {
         }
 
         int maxRepeatSite = 0;
-        for (int i = 0; i < repeatSite.length; i++)
-        {
-            maxRepeatSite = repeatSite[i] > maxRepeatSite ? repeatSite[i] : maxRepeatSite;
+        for (int i = 0; i < repeatSite.length; i++) {
+            maxRepeatSite = Math.max(repeatSite[i], maxRepeatSite);
         }
         int lastLength = s.length() - maxRepeatSite - 1;
-        if (lastLength > longestLength)
-        {
+        if (lastLength > longestLength) {
             longestLength = lastLength;
         }
         return longestLength;
+    }
+
+    int lengthOfLongestSubstringAns(String s) {
+        int max = 0;
+        int left = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            max = Math.max(max, i - left + 1);
+            map.put(s.charAt(i), i);
+        }
+        return max;
     }
 }
